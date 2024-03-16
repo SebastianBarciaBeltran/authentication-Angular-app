8a1e92ff0ea84d54148c02bb7909ba614d64ae2d
Sebastián Barcia Beltrán <sebastian.barcia@bosonit.com>
2024-03-16 13:40:31 +0100

    chore: create core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+// Angular imports
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+// Module inner imports
+// Third-party libraries imports
+// Services
+// Components
+// Standalone components
+// Interfaces
+// Others
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+})
+export class CoreModule {}
